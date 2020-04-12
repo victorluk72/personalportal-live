437adcc32f9d30ae9dc74a5d8311db1f2ff4148c
Viktor Lukyanenko <victorluk72@yahoo.ca>
2020-04-12 14:43:10 -0400

    make it better

--- personalportal/settings.py
+++ personalportal/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',  cast=bool)
 
-ALLOWED_HOSTS = ['vl-personalportal.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['vl-personalportal.herokuapp.com', 'localhost', 'victorluk.herokuapp.com']
 
 
 # Application definition
